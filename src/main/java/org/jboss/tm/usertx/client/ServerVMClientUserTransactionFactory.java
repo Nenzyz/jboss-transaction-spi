bc317ea3c46cbc9b95b0b44dd95b6ba2b108684e
Yan Valuyskiy <yan.valuyskiy@true-item.com>
2017-12-26 16:31:28 +0200

    Intermediate results (initial)

--- src/main/java/org/jboss/tm/usertx/client/ServerVMClientUserTransactionFactory.java
+++ src/main/java/org/jboss/tm/usertx/client/ServerVMClientUserTransactionFactory.java
@@ -22,13 +22,73 @@
 package org.jboss.tm.usertx.client;
 
 import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
+import org.jboss.tm.TransactionPropagationContextUtil;
+import javax.transaction.UserTransaction;
+import javax.naming.NamingException;
+import javax.naming.Reference;
+
+import org.jboss.logging.Logger;
+
 public class ServerVMClientUserTransactionFactory implements ObjectFactory {
+
+    /** The log */
+    private static final Logger log = Logger.getLogger(ServerVMClientUserTransactionFactory.class);
+
+    /**
+     *  The <code>UserTransaction</code> this factory will return.
+     *  This is evaluated lazily in {@link #getUserTransaction()}.
+     */
+    static private UserTransaction userTransaction = null;
+
+    /**
+     *  Get the <code>UserTransaction</code> this factory will return.
+     *  This may return a cached value from a previous call.
+     */
+    static private UserTransaction getUserTransaction()
+    {
+        if (userTransaction == null) {
+            // See if we have a local TM
+            try {
+                 new InitialContext().lookup("java:/TransactionManager");
+
+                // We execute in the server.
+                userTransaction = ServerVMClientUserTransaction.getSingleton();
+//            } catch (NamingException ex) {
+            } catch (Exception ex) {
+                // We execute in a stand-alone client.
+                ClientUserTransaction cut = ClientUserTransaction.getSingleton();
+
+                // Tell the proxy that this is the factory for
+                // transaction propagation contexts.
+                TransactionPropagationContextUtil.setTPCFactory(cut);
+                userTransaction = cut;
+            }
+        }
+        return userTransaction;
+    }
+
     @Override
     public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable environment) throws Exception {
-        return ServerVMClientUserTransaction.getSingleton();
+        // return ServerVMClientUserTransaction.getSingleton();
+        Reference ref = (Reference)obj;
+ 
+        if (!ref.getClassName().equals(ClientUserTransaction.class.getName()))
+            return null;
+
+        log.info(this + " environment " + environment.toString());
+
+        if (environment.containsKey("java.naming.provider.url")) {
+            ClientUserTransaction localClientUserTransaction = ClientUserTransaction.getSingleton();
+
+            TransactionPropagationContextUtil.setTPCFactory(localClientUserTransaction);
+            return localClientUserTransaction;
+        }
+
+        return getUserTransaction();
     }
 }
